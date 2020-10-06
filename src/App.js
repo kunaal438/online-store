a9df9f4d1b922e7398f79519ac8610348ed7f238
kunaal438 <kunaal438@gmail.com>
2020-10-06 16:58:34 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import HomePage from './homePage.component';
+
+// components
+
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+  );
+}
+
+export default App;
