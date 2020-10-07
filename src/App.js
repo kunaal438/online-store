e8e7f0a09a722a835acb00828e7e083c145b842e
kunaal438 <kunaal438@gmail.com>
2020-10-07 13:52:46 +0530

    react router dom installed and made routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homePage.component';
 
 // components
 
+const HatsPage = () => (
+  <div>
+    <h2>hats page</h2>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
