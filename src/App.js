31ef2695f54c9715e40742ac4d97427d33e9ed3c
kunaal438 <kunaal438@gmail.com>
2020-10-07 18:37:25 +0530

    navigation component created

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homePage.component';
 import ShopPage from './pages/shopPage/shopPage.component';
+import Navbar from './components/navbar/navbar.component';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './navbar.styles.scss';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg.svg';
+
+const Navbar = () => (
+    <nav className="navbar">
+        <Link to="/" className="logo">
+            <Logo  className="svg"/>
+        </Link>
+
+        <div className="navigation-links-parent">
+            <ul className="nav-ul">
+                <li>
+                    <Link to="/shop">shop</Link>
+                </li>
+                <li>
+                    <a href="http://kunaal.herokuapp.com/contact" target="_blank">contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+)
+
+export default Navbar;
